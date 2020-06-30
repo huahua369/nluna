a896817f2ae030ce2d7603dbe4aedbba2976610e
华仔 <188665600@qq.com>
2020-06-30 11:37:29 +0800

    1137

---
+++ src/vs/mag_promise.cpp
@@ -0,0 +1,168 @@
+﻿#include "mag_promise.h"
+
+namespace hz {
+	template<typename ...Args>
+	promise_cx::promise_cx(Args ...args)
+	{
+		auto n = sizeof...(args);
+		cbs = { args... };
+	}
+	template<typename ...Args>
+	void promise_cx::insert(Args ...args)
+	{
+		auto n = sizeof...(args);
+		vec_t m{ args... };
+		if (m.size())
+		{
+			cbs.insert(cbs.end(), m.begin(), m.end());
+		}
+	}
+
+
+	promise_cx::~promise_cx()
+	{
+
+	}
+
+	bool promise_cx::isdone() {
+		return idx > 0 && idx == cbs.size();
+	}
+
+	// 重置
+	void promise_cx::clear()
+	{
+		idx = 0;
+		cbs.clear();
+		is_next = true;
+	}
+
+
+
+	// 添加异步任务
+	promise_cx& promise_cx::then(std::function<void(bool* is_done)> cb)
+	{
+		cbs.push_back(cb);
+		return *this;
+	}
+
+	// 异步线程执行
+	void promise_cx::call()
+	{
+		// is_next==true则继续执行
+		for (; is_next && idx < cbs.size();)
+		{
+			auto& cb = cbs[idx++];
+			if (cb)
+			{
+				is_next = false;
+				cb(&is_next);
+			}
+		}
+	}
+	// --------------------------------------------------------------------------------
+
+	void promise_run::push(promise_cx* p)
+	{
+		if (p)
+		{
+			p->is() ? rs.push(p) : ws.push(p);
+		}
+	}
+
+	size_t promise_run::run(int ms)
+	{
+		std::queue<promise_cx*> t;
+		do {
+			for (; rs.size();)
+			{
+				auto p = rs.front();
+				rs.pop();
+				if (p)
+				{
+					p->call();
+					if (!p->isdone())
+					{
+						ws.push(p);
+					}
+				}
+			}
+			for (; ws.size();)
+			{
+				auto p = ws.front();
+				ws.pop();
+				if (p)
+				{
+					p->is() ? rs.push(p) : t.push(p);
+				}
+			}
+			if (t.size())
+			{
+				ws.swap(t);
+			}
+			if (ms > 0)
+			{
+				sleep(ms);
+			}
+		} while (ms > 0);
+		return rs.size() + ws.size();
+	}
+
+	void promise_run::sleep(int s)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(s));
+	}
+
+	// 异步测试
+
+	void promise_run::test()
+	{
+		bool b = false;
+		bool* bp = nullptr;
+		promise_cx pe1([](bool* d) { printf("pe1第1步\n"); *d = true; }
+		, [](bool* d) { printf("pe1第2步\n"); *d = true; }
+		, [](bool* d) { printf("pe1第3步\n"); *d = true; });
+
+		pe1.insert([](bool* d) { printf("pe1第5步\n"); *d = true; }
+		, [](bool* d) { printf("pe1第6步\n"); *d = true; });
+
+		promise_cx pe2;
+		mp_timer ts;
+		{
+			pe2.then([&](bool* d) {
+				int ems = ts.elapsed();
+				printf("pe2第1步:%d ms\n", ems); ts.reset(); bp = d; b = true;
+				})
+				.then([&](bool* d) {
+					int ems = ts.elapsed();
+					printf("pe2第2步:%d ms\n", ems); ts.reset(); bp = d; b = true;
+					})
+					.then([&](bool* d) {
+						int ems = ts.elapsed();
+						printf("pe2第3步:%d ms\n", ems); bp = d; b = true;
+						});
+					// 等待另一个异步线程
+					std::thread([&]() {
+						while (1) {
+							sleep(600);
+							if (b) {
+								if (bp)
+									*bp = true;
+								b = false;
+							}
+						}}).detach();
+		}
+		ts.reset();
+		// 执行异步操作
+		promise_run pr;
+		pr.push(&pe1);
+		pr.push(&pe2);
+		for (;;)
+		{
+			//pe1.call();pe2.call();
+			pr.run(0);
+			sleep(1);
+		}
+	}
+
+}
+//!hz
---
+++ src/vs/mag_promise.h
@@ -0,0 +1,99 @@
+﻿#pragma once
+
+#include <thread>
+#include <chrono>
+#include <functional>
+#include <vector>
+#include <queue>
+
+namespace hz {
+
+	class mp_timer
+	{
+	private:
+		std::chrono::time_point<std::chrono::high_resolution_clock> m_begin;
+	public:
+		mp_timer() : m_begin(std::chrono::high_resolution_clock::now())
+		{
+		}
+
+		~mp_timer()
+		{
+		}
+
+		void reset() { m_begin = std::chrono::high_resolution_clock::now(); }
+
+		//输出毫秒
+		uint64_t elapsed() const
+		{
+			return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - m_begin).count();
+		}
+		//微秒
+		uint64_t elapsed_micro() const
+		{
+			return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - m_begin).count();
+		}
+	private:
+
+	};
+
+	/*
+		todo 异步任务
+		串联、并联
+		is_next为true则继续执行下一下任务
+	*/
+	class promise_cx
+	{
+	public:
+		using vec_t = std::vector<std::function<void(bool* is_done)>>;
+	private:
+		bool is_next = true;
+		size_t idx = 0;
+		vec_t cbs;
+	public:
+		promise_cx()
+		{
+		}
+		template<typename... Args>
+		promise_cx(Args... args);
+		virtual ~promise_cx();
+		bool is() { return is_next; }
+		bool isdone();
+		// 重置
+		void clear();
+
+		template<typename... Args>
+		void insert(Args... args);
+
+		// 添加异步任务
+		promise_cx& then(std::function<void(bool* is_done)> cb);
+		// 异步线程执行
+		void call();
+	public:
+
+	};
+	class promise_run
+	{
+	private:
+		std::queue<promise_cx*> rs;
+		std::queue<promise_cx*> ws;
+
+	public:
+		promise_run()
+		{
+		}
+
+		virtual ~promise_run()
+		{
+		}
+	public:
+		void push(promise_cx* p);
+		size_t run(int ms);
+
+		static void sleep(int s);
+	public:
+		// 异步测试
+		static void test();
+	};
+}
+//!hz
